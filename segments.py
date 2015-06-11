acd9444b19bc8959b6bc9a7d2a2103ccd098600c
Julien Roche <julienr@interaction-multimedia.com>
2015-06-11 12:21:34 +0200

    (master) remove test method

--- segments.py
+++ segments.py
@@ -36,46 +36,10 @@ class CakePHPSegment(Segment):
             else:
                 raise
 
-    def test(self, cake_core_include_path):
-        try:
-            if os.path.isfile(cake_core_include_path + "/Cake/VERSION.txt") == False:
-                return None
-
-            status, cake_version = commands.getstatusoutput("awk '/./{line=$0} END{print line}' " + cake_core_include_path + "/Cake/VERSION.txt")
-
-            if cake_version != '':
-                return [{
-                    'contents': str(cake_version),
-                    'highlight_groups': ['version']
-                }]
-            else:
-                return None
-
-        except OSError as e:
-            if e.errno == 2:
-                pass
-            else:
-                raise
-
     def __call__(self, pl, segment_info, cake_core_include_path='lib'):
 
         return self.version(cake_core_include_path)
 
 cakephp = with_docstring(CakePHPSegment(),
-'''Return the status of a Git working copy.
-
-It will show the branch-name, or the commit hash if in detached head state.
-
-It will also show the number of commits behind, commits ahead, staged files,
-unmerged files (conflicts), changed files, untracked files and stashed files
-if that number is greater than zero.
-
-:param bool use_dash_c:
-    Call git with ``-C``, which is more performant and accurate, but requires git 1.8.5 or higher.
-    Otherwise it will traverse the current working directory up towards the root until it finds a ``.git`` directory, then use ``--git-dir`` and ``--work-tree``.
-    True by default.
-
-Divider highlight group used: ``gitstatus:divider``.
-
-Highlight groups used: ``gitstatus_branch_detached``, ``gitstatus_branch_dirty``, ``gitstatus_branch_clean``, ``gitstatus_branch``, ``gitstatus_behind``, ``gitstatus_ahead``, ``gitstatus_staged``, ``gitstatus_unmerged``, ``gitstatus_changed``, ``gitstatus_untracked``, ``gitstatus_stashed``, ``gitstatus``.
+'''Return the CakePHP version number.
 ''')
