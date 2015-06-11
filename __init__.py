61676ff6072d44f98b67f7ddd5d05ab52c0582e6
Julien Roche <julienr@interaction-multimedia.com>
2015-06-11 11:04:55 +0200

    (master) use a class instead directly method + plugin can be override

--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
-import pkg_resources
-pkg_resources.declare_namespace(__name__)
+#import pkg_resources
+#pkg_resources.declare_namespace(__name__)
+from .segments import cakephp
---
+++ segments.py
@@ -0,0 +1,62 @@
+from __future__ import absolute_import
+from subprocess import Popen, PIPE
+from powerline.theme import requires_segment_info
+
+from powerline.segments import Segment, with_docstring
+from subprocess import PIPE, Popen
+import os, re, string
+
+import os
+import sys
+import subprocess
+import commands
+from xml.dom import minidom
+
+@requires_segment_info
+class CakePHPSegment(Segment):
+
+    def version(self, path):
+        print path
+        try:
+            if os.path.isfile(path + "/Cake/VERSION.txt") == False:
+                return None
+
+            status, cake_version = commands.getstatusoutput("awk '/./{line=$0} END{print line}' " + path + "/Cake/VERSION.txt")
+
+            if cake_version != '':
+                return [{
+                    'contents': str(cake_version),
+                    'highlight_groups': ['version']
+                }]
+            else:
+                return None
+
+        except OSError as e:
+            if e.errno == 2:
+                pass
+            else:
+                raise
+
+
+    def __call__(self, pl, segment_info, path='lib'):
+
+        return self.version(path)
+
+cakephp = with_docstring(CakePHPSegment(),
+'''Return the status of a Git working copy.
+
+It will show the branch-name, or the commit hash if in detached head state.
+
+It will also show the number of commits behind, commits ahead, staged files,
+unmerged files (conflicts), changed files, untracked files and stashed files
+if that number is greater than zero.
+
+:param bool use_dash_c:
+    Call git with ``-C``, which is more performant and accurate, but requires git 1.8.5 or higher.
+    Otherwise it will traverse the current working directory up towards the root until it finds a ``.git`` directory, then use ``--git-dir`` and ``--work-tree``.
+    True by default.
+
+Divider highlight group used: ``gitstatus:divider``.
+
+Highlight groups used: ``gitstatus_branch_detached``, ``gitstatus_branch_dirty``, ``gitstatus_branch_clean``, ``gitstatus_branch``, ``gitstatus_behind``, ``gitstatus_ahead``, ``gitstatus_staged``, ``gitstatus_unmerged``, ``gitstatus_changed``, ``gitstatus_untracked``, ``gitstatus_stashed``, ``gitstatus``.
+''')
